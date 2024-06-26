fae0f6e5239969d7237926f1a42a179dc4e32145
Martin93K <ck313383@f1365.ort.edu.uy>
2024-06-26 14:51:23 -0300

    Entrega de Martin Kachechca - 313383

--- entrega/entrega.sh
+++ entrega/entrega.sh
@@ -1,2 +1,31 @@
-# Entrega Nombre Apellido - NumeroDeEstudiante
+# Entrega Martin Kachechca - 313383
+
+function volver(){
+    echo -e "Menú Principal[0]"
+    read op
+    option=$op;
+
+    case $option in
+        0) clear; menu;;
+        *) clear; echo -e "Se ha igresado un valor no válido";
+            volver;;
+    esac
+}
+
+function menu(){
+    echo "Ingrese el menú que desea utilizar:"
+    echo -e "Uno: [1] \nDos: [2] \nTres: [3]\nSalir: [0]"
+    read op
+    option=$op
+    
+    case $option in
+        0) clear; echo "Instancia Finalizada"; exit ;;
+        1) clear; echo "La opción fue:" $option; volver;;
+        2) clear; echo "La opción fue:" $option; volver ;;
+        3) clear; echo "La opción fue:" $option; volver ;;
+        *) clear; echo -e "Se ha igresado un valor no válido";
+            menu ;;
+    esac
+}
+menu
 
